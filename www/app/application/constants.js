c324cdc0e98d00141796a9075bc9b534d43f28aa
Vlad Bezden <vlad.bezden@gmail.com>
2015-03-11 22:57:10 -0400

    removed current date from forecast

--- www/app/application/constants.js
+++ www/app/application/constants.js
@@ -6,7 +6,7 @@
     'use strict';
 
     app.constant('apiEndpoints', {
-        FORECAST_API: 'https://api.forecast.io/forecast/1e790b156a6df458aaf21fed582486e7/',
+        FORECAST_API: '/api/forecast/',
         GOOGLE_MAPS_API: 'http://maps.googleapis.com/maps/api/geocode/json'
     });
 
